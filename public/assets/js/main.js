514eff62ddf2e465df9caf9df934307b28655fd9
Radhe Shyam (Unifyed) <radhe_shyam@unifyed.com>
2019-08-04 16:34:10 -0600

    component props

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,14 +8,15 @@ class LikeButton extends React.Component {
     }
 
     render() {
+        console.log('like button props=>>', this.props);
         if (this.state.liked) {
-            return 'You liked this.';
+            return 'You liked this.' + this.props.mi;
         }
 
         return e(
             'button',
             { onClick: () => this.setState({ liked: true }) },
-            'Like'
+            'Like' + this.props.mi
         );
     }
 }
@@ -29,20 +30,31 @@ class Hello extends React.Component {
         return e(
             'label',
             null,
-            e(
-                'b',
-                null,
-                xyz.map((ele, i) => e(
-                        'a',
-                        {
-                            key: i,
-                            href: "radhe",
-                            class: "classname"
-                        },
-                        "borrow" + ele
+            [
+                e(
+                    'b',
+                    { key: Math.random() },
+                    xyz.map((ele, i) => [
+                        e(
+                            'a',
+                            {
+                                key: Math.random(),
+                                href: "radhe",
+                                className: "classname"
+                            },
+                            "borrow" + ele
+                        ),
+                        e(LikeButton, { mi: 'how', key: Math.random() }),
+                        e('br', { key: Math.random() })
+                    ]
                     )
+                ),
+                e(
+                    'i',
+                    { key: Math.random() },
+                    'are u sleeping baby?'
                 )
-            )
+            ]
         );
     }
 }
